ba92c4e17fc05061ee5dbf9717275240b9657a22
m-motawea <magedmotawea@gmail.com>
2019-11-06 17:36:27 +0200

    support lists in vars files

--- lib/var_parser.py
+++ lib/var_parser.py
@@ -7,6 +7,12 @@ class VarParser(object):
         self.config.read(vars_file_path)
         self.sections = self.config.sections()
 
+    def get_variable_value(self, var):
+        if len(var.split(",")) > 1:
+            return var.split(",")
+        else:
+            return var
+
     def build_var_tree(self, target_groups):
         section_var_tree = {}
         group_vars = {}
@@ -14,7 +20,7 @@ class VarParser(object):
         if "all" in self.config:
             # global variables
             for var in self.config["all"]:
-                all_var[var] = self.config["all"][var]
+                all_var[var] = self.get_variable_value(self.config["all"][var])
 
         for section in self.sections:
             if section == "all":
@@ -29,13 +35,13 @@ class VarParser(object):
                     section_var_tree[group][host] = {}
                     # device specific variables
                     for var in self.config[section]:
-                        section_var_tree[group][host][var] = self.config[section][var]
+                        section_var_tree[group][host][var] = self.get_variable_value(self.config[section][var])
                 else:
                     # group variables
                     section_var_tree[section] = {}
                     group_vars[section] = {}
                     for var in self.config[section]:
-                        group_vars[section][var] = self.config[section][var]
+                        group_vars[section][var] = self.get_variable_value(self.config[section][var])
 
         # add all hosts under a group to the var tree
         for target_group in target_groups:
